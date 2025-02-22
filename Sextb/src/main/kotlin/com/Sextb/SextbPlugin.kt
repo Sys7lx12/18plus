b24f85289ad9a4dde236db5fe8b0f61f283d891e
Sys7lx12 <50859346+Sys7lx12@users.noreply.github.com>
2025-02-22 15:25:50 +0700

    Update SextbPlugin.kt

--- Sextb/src/main/kotlin/com/Sextb/SextbPlugin.kt
+++ Sextb/src/main/kotlin/com/Sextb/SextbPlugin.kt
@@ -1,18 +1,18 @@
-package com.Sextb
+package com.Sextb // Khai báo package của class
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
-import com.Sextb.Stbturbo
-import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.extractors.Wishonly
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin // Import annotation để đánh dấu class là plugin
+import com.lagradost.cloudstream3.plugins.Plugin // Import class Plugin, class cha của SextbPlugin
+import android.content.Context // Import Context để truy cập tài nguyên Android
+import com.Sextb.Stbturbo // Import extractor Stbturbo tự viết
+import com.lagradost.cloudstream3.extractors.StreamTape // Import extractor StreamTape
+//import com.lagradost.cloudstream3.extractors.Wishonly // Import extractor Wishonly (đã được comment)
 
-@CloudstreamPlugin
-class SextbPlugin: Plugin() {
-    override fun load(context: Context) {
-        registerMainAPI(SextbProvider())
-        registerExtractorAPI(StreamTape())
-        //registerExtractorAPI(Wishonly())
-        registerExtractorAPI(Stbturbo())
+@CloudstreamPlugin // Annotation đánh dấu class là plugin của Cloudstream
+class SextbPlugin: Plugin() { // Khai báo class SextbPlugin kế thừa từ class Plugin
+    override fun load(context: Context) { // Hàm load được gọi khi plugin được tải
+        registerMainAPI(SextbProvider()) // Đăng ký API chính của plugin, sử dụng SextbProvider
+        registerExtractorAPI(StreamTape()) // Đăng ký extractor StreamTape
+        //registerExtractorAPI(Wishonly()) // Đăng ký extractor Wishonly (đã được comment)
+        registerExtractorAPI(Stbturbo()) // Đăng ký extractor Stbturbo
     }
 }
